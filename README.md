2eeffa71811b9456110542f083736ef0ec1c89cc
Giancarlo Valencia <giancarlovs2891@gmail.com>
2020-04-13 16:22:36 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# CONEXIS
+
+Back-end repository
+
+Install instruction:
+
+1. npm Install
+
+2. create .env like this:
+    ```
+    KNACK_APP_ID=value
+    KNACK_API_KEY=value
+    ```
---
+++ server/api/api.js
@@ -0,0 +1,82 @@
+'use strict';
+// Load modules
+
+const Moment = require('moment');
+const Promise = require('bluebird');
+// const Request = require('request-promise');
+const Request = require('promise-request-retry');
+const UrlUnshort = require('url-unshort')();
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_ID, process.env.TWILIO_AUTH_TOKEN);
+
+// Declare internals
+
+const internals = {
+    KNACK_OBJECTS_IDS: {
+        BuyerInvoice: 'object_45',
+        Contract: 'object_6',
+        ContractInvoice: 'object_35',
+        SupplierInvoice: 'object_44',
+        TimeExpense: 'object_33',
+        lineItems: 'object_37',
+        contractAmendment: 'object_47',
+        proposalObject: 'object_12',
+        Worker: 'object_30'
+    },
+    CONCURRENCY_OPTS: {
+        concurrency: 1
+    }
+};
+
+exports.plugin = {
+    name: 'api',
+    register: function (server) {
+
+        const { Knack } = server.plugins;
+        // const KNACK_OBJECTS_IDS = Knack.objects();
+
+        server.route({
+            method: 'POST',
+            path: '/twiliosms',
+            handler: async function (request, h) {
+
+                const payload = request.payload;
+
+                return await client.messages
+                .create({
+                    body: payload.message,
+                    from: '+12056560386',
+                    to: `+${payload.to}`
+                 });
+            }
+        });
+
+        server.route({
+            method: 'GET',
+            path: '/test',
+            handler: async function (request, h) {
+
+                console.log(`http://httpstat.us/${request.query.status}?sleep=${request.query.sleep}`);
+
+                const sArray = [];
+
+                for (let i = 0; i < 52; i++) {
+                    const status = 400 + i;
+                    sArray.push(status);
+                }   
+
+                console.log(sArray)
+                
+
+                const options = {
+                    uri: `http://httpstat.us/${request.query.status}?sleep=${request.query.sleep}`,
+                    method: 'GET',
+                    retry : 3,
+                    accepted: sArray,
+                    delay: 5000
+                };
+            
+                return await Request(options);
+            }
+        });
+    }
+};
